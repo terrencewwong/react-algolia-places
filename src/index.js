b1f0d83c6e6b8926fff3312b408115c51a2af197
Terrence Wong <wongterrencew@gmail.com>
2018-05-18 18:41:03 +0200

    make an input that can search algolia

--- src/index.js
+++ src/index.js
@@ -1,5 +1,44 @@
 // @flow
 import React from 'react'
+import algoliasearch from 'algoliasearch/src/browser/builds/algoliasearchLite.js'
 
-const Places = () => <div>Hello</div>
+const placesClient = algoliasearch.initPlaces()
+// placesClient.as.addAlgoliaAgent(`@terrencewwong/react-algolia-places ${version}`);
+
+class Places extends React.Component<
+  *,
+  {
+  suggestions: Object[],
+  value: string
+  }
+  > {
+  state = {
+    suggestions: [],
+    value: ''
+  }
+
+  handleChange = async (e: SyntheticInputEvent<*>) => {
+    const value = e.target.value
+    this.setState({ value })
+
+    const suggestions = await placesClient.search({
+      query: value
+    })
+
+    this.setState({ suggestions })
+  }
+
+  render () {
+    return (
+      <React.Fragment>
+        <input
+          onChange={this.handleChange}
+          type='text'
+          value={this.state.value}
+        />
+        <pre>{JSON.stringify(this.state.suggestions, null, 2)}</pre>
+      </React.Fragment>
+    )
+  }
+}
 export default Places
