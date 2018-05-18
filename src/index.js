c513e8db29e831aa80206fe4a4b1fa58a62ea7da
Terrence Wong <wongterrencew@gmail.com>
2018-05-18 18:10:54 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+// @flow
+import React from 'react'
+
+const Places = () => <div>Hello</div>
+export default Places
---
+++ stories/index.stories.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import Places from '../src'
+
+storiesOf('Places', module).add('default', () => <Places />)
